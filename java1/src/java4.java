68b4a8b37bd4563d312dcbff4755c9d53b6d316d
“viktorcvp” <dffggg2013@yandex.by>
2023-01-31 16:27:56 +0300

    now

---
+++ java1/src/java4.java
@@ -0,0 +1,22 @@
+public class java4 {
+        static int countPaths(int x, int y, int N){
+
+            if (x == N - 1 && y == N - 1) {
+                return 1;
+            }
+            if (x > N - 1 || y > N - 1) {
+                return 0;
+            }
+            return countPaths (x + 1, y, N)+ countPaths(x, y + 1, N);
+        }
+        public static void main(String[] args){
+            int res =  countPaths(1, 1, 8);
+            System.out.println(res);
+        }
+    }
+
+
+
+
+
+
